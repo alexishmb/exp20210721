5d0e6e6ae47f971ddcdc700a92f0bc23e1006744
alexishmb <hmantari@gmail.com>
2021-07-22 20:42:36 -0500

    variante05

--- oauth/src/main/java/com/cjavaperu/oauth/service/IUsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.cjavaperu.oauth.service;
-
-import com.cjavaperu.usuarioscommons.model.entity.Usuario;
-
-public interface IUsuarioService {
-	
-	public Usuario findByUsername(String username);
-
-}
